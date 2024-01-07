4bdfcd3f6dc0dbb04e4f2e1b30340d4dd3cf8a55
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-07 21:39:53 +0530

    Debugged some structure code and added some general HTML element capturing scripts...

---
+++ script.js
@@ -0,0 +1,8 @@
+const currencyOneEl = document.getElementById("currency-one");
+const currencyTwoEl = document.getElementById("currency-two");
+const amountOneEl = document.getElementById("amount-one");
+const amountTwoEl = document.getElementById("amount-two");
+const swap = document.getElementById("swap");
+const rateEl = document.getElementById("rate");
+
+
