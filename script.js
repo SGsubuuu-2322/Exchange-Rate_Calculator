59ca275bb0429ad2bc5bd414b5a599a146bcd7f4
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-01-07 22:41:31 +0530

    Added the scripts for fetching the selected currency value...

--- script.js
+++ script.js
@@ -5,4 +5,23 @@ const amountTwoEl = document.getElementById("amount-two");
 const swap = document.getElementById("swap");
 const rateEl = document.getElementById("rate");
 
+function calculate() {
+  const currencyOneElValue = currencyOneEl.value;
+  const currencyTwoElValue = currencyTwoEl.value;
+  fetch(
+    `https://v6.exchangerate-api.com/v6/8f4dda0e6e6905cb5f1b1000/latest/${currencyOneElValue}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const rate = data.conversion_rates[currencyTwoElValue];
 
+      rateEl.innerText = `1 ${currencyOneElValue} = ${rate} ${currencyTwoElValue} `;
+    });
+}
+
+currencyOneEl.addEventListener("change", calculate);
+amountOneEl.addEventListener("input", calculate);
+currencyTwoEl.addEventListener("change", calculate);
+amountTwoEl.addEventListener("input", calculate);
+
+calculate();
